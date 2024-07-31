23ed96a3541e4e12c9a650b6fe2005d08986f2b2
Jose Arturo Garcia <jose.arturo.garcia@gmail.com>
2024-07-30 22:11:58 -0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,11 @@
 import os
 import shutil
+from datetime import datetime
+
+current_year = datetime.now().year
+
+context = {{ cookiecutter }}
+context["_year"] = current_year
 
 try:
     os.symlink('doc/index.rst', 'README.rst')
